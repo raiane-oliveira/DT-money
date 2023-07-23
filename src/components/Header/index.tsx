27970ca5c0b928a3c6cafdca2dc158940463ad02
Raiane <raiane.oliveira404@gmail.com>
2023-07-23 10:49:04 -0300

    feat: accessible modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,10 @@ import {
   NewTransactionButton,
 } from './styles'
 
+import * as Dialog from '@radix-ui/react-dialog'
+
 import logoIgnite from '../../assets/ignite-simbol.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -16,7 +19,13 @@ export function Header() {
           <span>DT Monkey</span>
         </LogoContainer>
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
